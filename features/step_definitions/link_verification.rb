79e15acfb83635d30d4b2bb1a6d97acb703d8af3
Jeremy Walsh <jeremy_walsh@live.com>
2015-12-03 11:31:07 -0500

    link_verification refactoring

--- features/step_definitions/link_verification.rb
+++ features/step_definitions/link_verification.rb
@@ -9,9 +9,6 @@ end
 Then(/^I should see the social media links$/) do | links |
   #page.should have_text "Products"
   links.hashes.each do | link |
-    #puts link["url"]
-    #page.should have_link "https://www.facebook.com/Raven-Concealment-Systems-305851175686"
-    #page.should have_link("", {href: "https://www.facebook.com/Raven-Concealment-Systems-305851175686"})
     page.should have_link("", {href: link["url"]})
   end
 end
